d504c620e23523482b40b656af37f4092a598f03
ab8x1 <125adam123@gmail.com>
2023-11-01 17:06:56 +0100

    started new UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles/indexStyles.css"
+import lottie from 'lottie-web';
+
+const animationElement = document.getElementById("animation");
+
+lottie.loadAnimation({
+    container: animationElement, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/public/animation.json' // the path to the animation json
+  });
